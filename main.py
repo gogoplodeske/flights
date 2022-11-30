ec8de21f93e59ef55b8790ce5261ee43211456ec
gogoplodeske <plodeske.g.21@gmail.com>
2022-11-30 19:16:18 +0200

    building the processer and GEt/Post request

--- main.py
+++ main.py
@@ -1,16 +1,64 @@
-# This is a sample Python script.
+from flask import Flask, jsonify
+from flask_restful import Resource, Api, reqparse
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
+api = Api(app)
+
+data_arg = reqparse.RequestParser()
+data_arg.add_argument("Arrival", type=str, help="Enter arrival")
+data_arg.add_argument("Departure", type=str, help="Enter departure")
+data_arg.add_argument("success", type=str, default="fail")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def to_time(x):
+    return pd.to_datetime(x.strip(), format='%H:%M')
 
 
-# Press the green button in the gutter to run the script.
+def update_sucess(df):
+    df['success'] = 'fail'
+    m = df[(((df['Departure'].apply(to_time) - df['Arrival'].apply(to_time)).dt.total_seconds() / 60) >= 180)].index
+    df.loc[m[:20], 'success'] = "success"
+    df['arrival_date'] = df['Arrival'].apply(to_time)
+    df.sort_values(by='arrival_date', inplace=True)
+    df.drop(['arrival_date'], axis=1, inplace=True)
+    return df
+
+
+class flights(Resource):
+    def __init__(self):
+        self.df = pd.read_csv('data.csv')
+        self.df.columns = [x.strip() for x in self.df.columns]
+        self.df = update_sucess(self.df)
+
+    def get(self, ID):
+        # find data from csv based on user input
+        data_fount = self.df.loc[self.df['flight ID'] == ID].to_json(orient="records")
+        # return data found in csv
+        return jsonify({'message': data_fount})
+
+    # POST request on the url will hit this function
+    def post(self, ID):
+        # data parser to parse data from url
+        args = data_arg.parse_args()
+        args['flight ID'] = ID
+        # if ID is already present
+        if (self.df['flight ID'] == ID).any():
+            return jsonify({"message": 'ID already exist'})
+        else:
+            # Save data to csv
+            self.df = self.df.append(args, ignore_index=True)
+            if self.df['success'].value_counts()['success'] < 20:
+                self.df=self.df = update_sucess(self.df)
+            self.df.to_csv("data.csv", index=False)
+            print(self.df, self.df.value_counts())
+            return jsonify({"message": 'Done'})
+
+
+
+api.add_resource(flights, '/<ID>')
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run(debug=True)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
